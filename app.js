6f035a7e16d686409579a68c2659e12b5a71f01a
aditya <whitehatehack@gmail.com>
2024-09-04 22:37:55 +0530

    working with JWT

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-// const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-// app.use(cookieParser())
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
-    bcrypt.compare("polololo", "$2b$10$ypwqZvWP2JToecByU.yMYuhQTLLntkHsQJqepBL9g/lq7L3TuyzWO", function(err, result) {
-        console.log(result)
-    });
+  let token = jwt.sign({ email: "adityasingh@gmail.com" }, "secret");
+  res.cookie('token', token)
+  console.log(token)
+  res.send('done')
 });
+
+// app.get('/read', (req, res)=>{
+//     console.log(req.cookies.token)
+// })
+app.get('/read', (req, res)=>{
+    let data = jwt.verify(req.cookies.token, "secret")
+    console.log(data)
+})
 
 app.listen(3000, () => {
   console.log("server is running");
