e2f227f167efa032d03c972f66037a492544886a
aditya <whitehatehack@gmail.com>
2024-09-04 22:10:38 +0530

    compare hashed password and actual password DECRYPTION

--- app.js
+++ app.js
@@ -6,11 +6,9 @@ const bcrypt = require("bcrypt");
 // app.use(cookieParser())
 
 app.get("/", (req, res) => {
-  bcrypt.genSalt(10, function (err, salt) {
-    bcrypt.hash("polololo", salt, function (err, hash) {
-      console.log(hash);
+    bcrypt.compare("polololo", "$2b$10$ypwqZvWP2JToecByU.yMYuhQTLLntkHsQJqepBL9g/lq7L3TuyzWO", function(err, result) {
+        console.log(result)
     });
-  });
 });
 
 app.listen(3000, () => {
