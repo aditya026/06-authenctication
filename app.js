0973429b40c9e57bab40ea16371478fa60096925
aditya <whitehatehack@gmail.com>
2024-09-04 21:53:37 +0530

    bcrypt setup

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
-app.use(cookieParser())
+// app.use(cookieParser())
 
 app.get('/', (req,res)=>{
-    res.cookie('name', 'aditya')
     res.send('done')
 })
-app.get('/read', (req,res)=>{
-    cocnsole.log(req.cookies)
-    res.send('done')
-})
+
 
 app.listen(3000, ()=>{
     console.log("server is running")
