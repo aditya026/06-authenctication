f9182d641afa4e9df98dce8acdf5a09bd2c8b575
aditya <whitehatehack@gmail.com>
2024-09-04 22:04:43 +0530

    encrypt by bcrypt

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // const cookieParser = require('cookie-parser')
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
 
 // app.use(cookieParser())
 
-app.get('/', (req,res)=>{
-    res.send('done')
-})
+app.get("/", (req, res) => {
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash("polololo", salt, function (err, hash) {
+      console.log(hash);
+    });
+  });
+});
 
-
-app.listen(3000, ()=>{
-    console.log("server is running")
-})
+app.listen(3000, () => {
+  console.log("server is running");
+});
