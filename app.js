298d8d8db0cec3d0c3ca7eaad79679cb86a45751
aditya <whitehatehack@gmail.com>
2024-09-04 21:50:45 +0530

    cookie set & read cookie

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+const cookieParser = require('cookie-parser')
 const express = require('express')
 const app = express()
 
+app.use(cookieParser())
+
 app.get('/', (req,res)=>{
     res.cookie('name', 'aditya')
+    res.send('done')
+})
+app.get('/read', (req,res)=>{
+    cocnsole.log(req.cookies)
     res.send('done')
 })
 
